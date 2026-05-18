384e13876ba427b05bed89c25d4820d10b322952
irly.dev <irly.fizaharis.dev@gmail.com>
2026-05-18 16:50:42 +0700

    Finish view manage voter

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"e-voting/controller"
 	"e-voting/model"
-	"fmt"
+	"e-voting/view"
 )
 
 func main() {
-	var candidates model.Canditates = model.GenerateCandidatesData()
-	var voters model.Voters = model.GenerateVotersData()
-
-	fmt.Println(controller.InsertionCandidateSorting(candidates, 10, "asc", "name"))
-	fmt.Println(controller.SelectionVotersSorting(voters, 20, "desc", "name"))
+	model.Candidate = model.GenerateCandidatesData()
+	model.Voter = model.GenerateVotersData()
+	view.Home()
 }
