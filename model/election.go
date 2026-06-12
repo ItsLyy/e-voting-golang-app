fc8ddff1c9c993ebee4d754c85a130a53c986938
irly.dev <irly.fizaharis.dev@gmail.com>
2026-06-12 18:31:57 +0700

    Adding feature election on election menu

---
+++ model/election.go
@@ -0,0 +1,13 @@
+package model
+
+const NMAXELECTION = 10
+
+type ElectionData struct {
+	CandidateIndex int
+	TotalVote      int
+}
+
+type Elections struct {
+	Data   [NMAXELECTION]ElectionData
+	Length int
+}
